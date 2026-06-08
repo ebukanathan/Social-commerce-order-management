dd7d52c215346e1c31981005872f272c27ac2f2c
ebuka nathan <ebukanathan@gmail.com>
2026-06-08 23:06:34 +0100

    order loader

--- server/src/app.ts
+++ server/src/app.ts
@@ -22,7 +22,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(
   cors({
     origin: "http://localhost:3000", // your frontend
-    methods: ["GET", "POST", "PUT", "DELETE"],
+    methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
     credentials: true, // if using cookies/auth
   }),
 );
